fff3b023c134940ca77e5a86bf8d75c8dae6a7e5
MaksymKosenko <jgjghh2@gmail.com>
2021-04-10 21:13:08 +0300

    mfc

--- lib/global/custom_widgets/custom_drawer.dart
+++ lib/global/custom_widgets/custom_drawer.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:swipe_app/global/custom_widgets/custom_button.dart';
 import 'package:swipe_app/global/custom_widgets/logo.dart';
 import 'package:swipe_app/global/style/text_styles.dart';
-import 'package:swipe_app/global/user.dart';
 import 'package:swipe_app/screens/feed/landing_page.dart';
 import 'package:swipe_app/screens/maps/mfc_screen.dart';
 import 'package:swipe_app/screens/my_add/whole_ad_container.dart';
@@ -11,7 +9,7 @@ import 'package:swipe_app/screens/notary/notary_page.dart';
 import 'package:swipe_app/screens/profile/profile_screen.dart';
 
 Widget CustomDrawer(context, _userData){
-  final ConcreteUser user = Provider.of<ConcreteUser>(context);
+
 
   bool _isImageExist;
   if(_userData['profilePic'] == "null")
